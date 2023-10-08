5f7d4cf53dc3570a5164e667746b02eb59701cb7
BratsunDanila <Tyuiu.BratsunDA@outlook.com>
2023-10-08 20:23:36 +0500

    Task3

---
+++ Tyuiu.BratsunDA.Sprint1.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BratsunDA.Sprint1.Task3.V4.Lib;
+
+namespace Tyuiu.BratsunDA.Sprint1.Task3.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.75;
+            double y = 0.5;
+            int count = 7;
+            var res = ds.PurchaseAmount(x, y, count);
+            Assert.AreEqual(22.75, res);
+
+        }
+    }
+}
---
+++ Tyuiu.BratsunDA.Sprint1.Task3.V4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BratsunDA.Sprint1.Task3.V4.Lib;
+
+namespace Tyuiu.BratsunDA.Sprint1.Task3.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1| Выполнил: Брацун Д.А. | ПКТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил Брацун Д.А. | ПКТБ-23-1                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            double y;
+            int count;
+
+            Console.WriteLine("Введите стоимость тетради: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите стоимость обложки: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите количество комплектов: ");
+            count = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Стоимость покупки = " + ds.PurchaseAmount(x, y, count));
+
+            Console.ReadKey();
+        }
+    }
+}
