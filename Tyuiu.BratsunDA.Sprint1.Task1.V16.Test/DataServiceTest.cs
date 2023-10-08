45e68523f928d31116b60fc573acae3d10ab304c
BratsunDanila <Tyuiu.BratsunDA@outlook.com>
2023-10-08 17:57:10 +0500

    Task1

---
+++ Tyuiu.BratsunDA.Sprint1.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BratsunDA.Sprint1.Task1.V16.Lib;
+
+namespace Tyuiu.BratsunDA.Sprint1.Task1.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 3.0;
+            double y = 2.0;
+            double x = 1.0;
+            var res = ds.Calculate(a, x, y);
+            Assert.AreEqual(16.0, res);
+        }
+    }
+}
---
+++ Tyuiu.BratsunDA.Sprint1.Task1.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BratsunDA.Sprint1.Task1.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BratsunDA.Sprint1.Task1.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e5c0c94-6544-4901-8d2d-deae2763c16c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
